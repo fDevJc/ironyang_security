6cdd2242a9f98d0352d09c942f797eedd86f74ec
fDevJc <fdevjc@gmail.com>
2023-03-13 20:19:19 +0900

    시큐리티 설정 변경

--- src/main/java/ironyang/security1/config/SecurityConfig.java
+++ src/main/java/ironyang/security1/config/SecurityConfig.java
@@ -1,12 +1,9 @@
 package ironyang.security1.config;
 
-import ironyang.security1.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,47 +11,48 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.security.Provider;
+import ironyang.security1.service.UserService;
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final UserService userService;
+	private final UserService userService;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.formLogin().disable();
-        http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/users/**")
-                .authenticated()
-                .anyRequest()
-                .permitAll()
-                .and()
-                .addFilter(authenticationFilter())
-        ;
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.formLogin().disable();
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/users/**")
+			.authenticated()
+			.anyRequest()
+			.permitAll()
+			.and()
+			.addFilter(authenticationFilter())
+		;
+		return http.build();
+	}
 
-    public AuthenticationFilter authenticationFilter() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userService);
-        provider.setPasswordEncoder(new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return rawPassword.toString();
-            }
+	public AuthenticationFilter authenticationFilter() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userService);
+		provider.setPasswordEncoder(new PasswordEncoder() {
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return rawPassword.toString();
+			}
 
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return rawPassword.toString().equals(encodedPassword);
-            }
-        });
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				return rawPassword.toString().equals(encodedPassword);
+			}
+		});
 
-        AuthenticationManager manager = new ProviderManager(provider);
-        AuthenticationFilter filter = new AuthenticationFilter();
-        filter.setAuthenticationManager(manager);
-        return filter;
-    }
+		AuthenticationManager manager = new ProviderManager(provider);
+		AuthenticationFilter filter = new AuthenticationFilter();
+		filter.setAuthenticationManager(manager);
+		return filter;
+	}
 }
